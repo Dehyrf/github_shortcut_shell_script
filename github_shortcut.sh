eb21c2dff7ed652ca26987ad3a1650b98e1e183c
input null <Dehyrf@users.noreply.github.com>
2015-09-20 18:34:48 -0500

    Update github_shortcut.sh

--- github_shortcut.sh
+++ github_shortcut.sh
@@ -3,7 +3,7 @@
 read -e -p "Enter the path to the file: " -i "~/Downloads/github/" FILEPATH
 echo $FILEPATH
 
-touch ~/.bash_profile
-echo "alias github='cd $FILEPATH'" >> ~/.bash_profile
+touch ~/.bashrc
+echo "alias github='cd $FILEPATH'" >> ~/.bashrc
 
 echo Please log out and log back in, then use 'github' to change to github directory
